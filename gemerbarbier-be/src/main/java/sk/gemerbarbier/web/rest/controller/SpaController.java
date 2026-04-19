484c0303624b51df1af5524ce97eca948a198909
Marko Moravčík <moravcik@essential-data.sk>
2026-04-19 21:07:06 +0200

    spa config removed, spa controller added again

---
+++ gemerbarbier-be/src/main/java/sk/gemerbarbier/web/rest/controller/SpaController.java
@@ -0,0 +1,17 @@
+package sk.gemerbarbier.web.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SpaController {
+
+  @GetMapping(value = {
+      "/",
+      "/admin",
+      "/admin/**"
+  })
+  public String forward() {
+    return "forward:/index.html";
+  }
+}
