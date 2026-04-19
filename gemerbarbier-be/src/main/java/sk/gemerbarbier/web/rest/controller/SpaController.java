0318edc5549845ada4ad0fe7d56cd4af4c718347
Marko Moravčík <moravcik@essential-data.sk>
2026-04-19 20:52:49 +0200

    spa controller fix

--- gemerbarbier-be/src/main/java/sk/gemerbarbier/web/rest/controller/SpaController.java
+++ gemerbarbier-be/src/main/java/sk/gemerbarbier/web/rest/controller/SpaController.java
@@ -6,18 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpaController {
 
-  @RequestMapping(value = "/")
-  public String root() {
-    return "forward:/index.html";
-  }
-
-  @RequestMapping(value = "/{path:[^.]*}")
+  @RequestMapping(value = {
+      "/{path:[^\\.]*}",
+      "/{path:^(?!api$).*$}/**"
+  })
   public String redirect() {
-    return "forward:/index.html";
-  }
-
-  @RequestMapping(value = "/{path:^(?!api$).*$}/**")
-  public String redirectNested() {
     return "forward:/index.html";
   }
 }
